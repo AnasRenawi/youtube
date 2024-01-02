4cfabf36e7167b6d9a2e5d65da478932bd0b1388
anas renawi <anasrenawi@anass-MacBook-Pro.local>
2024-01-02 19:57:00 +0200

    new

--- src/components/Subscription.js
+++ src/components/Subscription.js
@@ -1,7 +1,84 @@
+import VideoCard from "../VideoCard";
+import './Subscription.css'
+
 const Subscription = () => {
     return (
-    <div>
-        <h1> Subscription</h1>
+    <div className="subscriptionVideos">
+        <h2> Subscription</h2>
+        <div className="subscriptionVideos_videos">
+        <VideoCard
+              title='The Game Awards Controversy, Mario RPG, PlayStation Portal, Last of Us 2 Remaster | Spawncast Ep 345'
+              views='45K Views'
+              timestamp='3 months ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZTHi1dBGJ24rCecx9lsJHBwHfz_ae_6ZnPNWweing=s88-c-k-c0x00ffffff-no-rj'
+              channel='Spwan Wave'
+              image='https://i.ytimg.com/an_webp/G_nChTPL_fs/mqdefault_6s.webp?du=3000&sqp=CPPvsawG&rs=AOn4CLA19APVhhZuSaGoaA3-5HZJTnIRtQ'
+              />
+          <VideoCard
+              title='How to Remember Things | Jim Kwik'
+              views='1.8M Views'
+              timestamp='5 months ago'
+              channelImage='https://yt3.ggpht.com/AmqWc1vXSvmPaiJC-6K5iUkKpryaD85UpaGkz8lTnDDmR8yhNGgIrjy_q524SUQqM4RHnGRNTA=s88-c-k-c0x00ffffff-no-rj'
+              channel='Jim Kwik'
+              image='https://i.ytimg.com/an_webp/Y7m59wRKXe4/mqdefault_6s.webp?du=3000&sqp=CPqhsqwG&rs=AOn4CLDNDH6UEgZjuAnDT69P7Q7lcqo8jQ'
+              />
+          <VideoCard
+              title='Film! Japanese snipers ambushed, met Chinese sharpshooter, instant headshots'
+              views='11M Views'
+              timestamp='5 months ago'
+              channelImage='https://yt3.ggpht.com/_8FXAB9dtFJ4jlAWuoAz9jyvQo9SqzcRgxcDEBja7YR7FHBzBAP6kbo0nhxCoDI_y3d8TDK2SD4=s88-c-k-c0x00ffffff-no-rj'
+              channel='Gun King'
+              image='https://i.ytimg.com/an_webp/HKQD5R35Reo/mqdefault_6s.webp?du=3000&sqp=CIz5sawG&rs=AOn4CLDlMv-FADADnCR2yX2_PS6I9p9mmw'
+              />
+          <VideoCard
+              title='NDERCOVER - Hollywood English Action Full Movie | Dwayne Johnson "The Rock" Superhit Action Movie'
+              views='6.2M Views'
+              timestamp='6 months ago'
+              channelImage='https://yt3.ggpht.com/qMMfadJ_zB6G_KzDafdy-jwgu7RqYjNlRqrNNlsmNgID4Hlui7ArZYmYUR-rBfYqW5CWedfYeZw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Hollywood English Collection'
+              image='https://i.ytimg.com/an_webp/II7GNZ-zS-U/mqdefault_6s.webp?du=3000&sqp=CLipsqwG&rs=AOn4CLCRidscetCtMePAYA_88fa6W8V6Pg'
+              />
+          <VideoCard
+              title='THE DETECTIVE - English Movie | Hollywood Blockbuster English Action Crime Movie HD | Jason Statham'
+              views='29M Views'
+              timestamp='7 months ago'
+              channelImage='https://yt3.ggpht.com/qMMfadJ_zB6G_KzDafdy-jwgu7RqYjNlRqrNNlsmNgID4Hlui7ArZYmYUR-rBfYqW5CWedfYeZw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Hollywood English Collection'
+              image='https://i.ytimg.com/an_webp/NAIzQFZACcw/mqdefault_6s.webp?du=3000&sqp=CISQsqwG&rs=AOn4CLCphcx7LYvwMBE2fBGnR1QrEKnyFg'
+              />
+          <VideoCard
+              title='Beethoven - Symphony No. 6 (Proms 2012)'
+              views='11M Views'
+              timestamp='10 months ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZQILSglbY8nP30I8FHR7tzHzQwjBZQ_UCRX=s88-c-k-c0x00ffffff-no-rj'
+              channel='Mandetriens'
+              image='https://i.ytimg.com/an_webp/aW-7CqxhnAQ/mqdefault_6s.webp?du=3000&sqp=COC6sqwG&rs=AOn4CLAvlb02JDfIIRnibPCj21YTAnMsrw'
+              />
+          <VideoCard
+              title='Dvořák: Symphony No. 9 "From the New World" / Karajan · Berliner Philharmoniker'
+              views='7.8M Views'
+              timestamp='11 months ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZSSUUqalpauE1a9Kr1uKQRN3cPJcUCVD9Z1LRHOXw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Berliner Philharmoniker'
+              image='https://i.ytimg.com/an_webp/P_1N6_O254g/mqdefault_6s.webp?du=3000&sqp=CIjAsqwG&rs=AOn4CLBll2PhXQLi37R_ac67NptHY4ukAQ'
+              />
+          <VideoCard
+              title='CAMPING in the RAIN [ Cosy Vibes, Relaxing sounds, Our dogs birthday, ASMR ]'
+              views='9M Views'
+              timestamp='1 year ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZTUy8vpaGO6DstYfJ-TICTuV6xqHHA4ulZ-NghdyA=s88-c-k-c0x00ffffff-no-rj'
+              channel='Go4x4'
+              image='https://i.ytimg.com/an_webp/t-amk-VJQXo/mqdefault_6s.webp?du=3000&sqp=CKaxsqwG&rs=AOn4CLDhJqRlo5u_s_Bt6KG5rkRY80gIew'
+              />
+          <VideoCard
+              title='BUILDING A TINY HOUSE - DIY Full Exterior Build'
+              views='17M Views'
+              timestamp='2 years ago'
+              channelImage='https://yt3.ggpht.com/zv_8nj95rvpayYjHZJ27SV-ZTAWEvE29PGyrOX-iKELK4hQT-xlSf8Ra0hjfoN5kV6pdVD0jbw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Drew Builds Stuff'
+              image='https://i.ytimg.com/an_webp/QDLLgV2fgx4/mqdefault_6s.webp?du=3000&sqp=CIjAsqwG&rs=AOn4CLDNaaWJOTOHDK01Vyeprbj7E1vONQ'
+              />
+        </div>
     </div>)
 }
 
--- src/components/Trending.js
+++ src/components/Trending.js
@@ -6,78 +6,126 @@ const Trending = () => {
     <div className='trendingVideos'>
         <h2> Trending</h2>
         <div className='trendingVideos_videos'>
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
-            <VideoCard
-            title='Become a Web Developer in 10 minutes'
-            views='2.3M Views'
-            timestamp='3 days ago'
-            channelImage='https://yt3.googleusercontent.com/ytc/APkrFKZffQLw2Cqp2yh5r_URmGxvsUDvhy3XlT6Hmq4U5Q=s176-c-k-c0x00ffffff-no-rj'
-            channel='anas renawi'
-            image='https://i.ytimg.com/vi/iGNq_poNvf8/hqdefault.jpg'
-            />
+                      <VideoCard
+              title='(PS5) EA FC 24 Looks AMAZING on PS5 | Realistic ULTRA Graphics Gameplay [4K 60FPS HDR] FIFA 24'
+              views='1.3M Views'
+              timestamp='3 days ago'
+              channelImage='https://yt3.googleusercontent.com/vUZNePGHpwyyhpCqFX1tWMgBtnrjhpzb17lzEHVlIKRodv91_ni6dT7p5yESvo99Jc4a7CilWg=s176-c-k-c0x00ffffff-no-rj'
+              channel='Champs Network'
+              image='https://i.ytimg.com/an_webp/brHDXJlbBbE/mqdefault_6s.webp?du=3000&sqp=CLSwsqwG&rs=AOn4CLB38kyt82lQVe-ahuVinlJuGnAIEg'
+              />
+          <VideoCard
+              title='How One of New York’s Best Chefs Runs His High Volume Michelin-Starred Restaurant — Mise En Place'
+              views='769K Views'
+              timestamp='10 days ago'
+              channelImage='https://yt3.googleusercontent.com/ytc/AIf8zZQ0SvaQUs4fzfKTNseYHFFTbpb-stwI7NjcillDBRM=s176-c-k-c0x00ffffff-no-rj'
+              channel='Eater'
+              image='https://i.ytimg.com/an_webp/LQdfE66rAwc/mqdefault_6s.webp?du=3000&sqp=CKWxsqwG&rs=AOn4CLBafK8dt6H2wzRSh-EAw_PI104EVg'
+              />
+          <VideoCard
+              title='iPhone vs Android (The Real Winner)!'
+              views='1.9M Views'
+              timestamp='14 days ago'
+              channelImage='https://yt3.ggpht.com/lkH37D712tiyphnu0Id0D5MwwQ7IRuwgQLVD05iMXlDWO-kDHut3uI4MgIEAQ9StK0qOST7fiA=s88-c-k-c0x00ffffff-no-rj'
+              channel='Marques Brownlee'
+              image='https://i.ytimg.com/an_webp/nHkKJ87FS6s/mqdefault_6s.webp?du=3000&sqp=CNOssqwG&rs=AOn4CLBif0bwgSBhZeK5_BBp9qxpQeBL3Q'
+              />
+          <VideoCard
+              title='Making the iPhone 15 Pro MKBHD Intro!'
+              views='272K Views'
+              timestamp='19 days ago'
+              channelImage='https://yt3.ggpht.com/3tdUDXkH3nTe7-FCRQkmFiRSzBXYgxUJMAzhORXDp5aGBi8JScXJeoT1TUaLlFJR4VKPZHbbc7A=s88-c-k-c0x00ffffff-no-rj'
+              channel='The Studio'
+              image='https://i.ytimg.com/an_webp/X1q3hH7aPs0/mqdefault_6s.webp?du=3000&sqp=CJ7zsawG&rs=AOn4CLBj_GWonndFstcfaKqbYRIrBOnPXQ'
+              />
+          <VideoCard
+              title='Homemade PIZZA from Handmade Oven with Refreshing WATERMELON juice! Life in the village'
+              views='3.1M Views'
+              timestamp='2 months ago'
+              channelImage='https://yt3.ggpht.com/HpbTU16qRa25us3SKwV5i3UTnXGpTeeRxjWq5XjqiLb6fjOreZh_K5z75ZRjiBM_Pz-gJB9lMP0=s88-c-k-c0x00ffffff-no-rj'
+              channel='WILDERNESS COOKING'
+              image='https://i.ytimg.com/an_webp/_odv9K2eGNU/mqdefault_6s.webp?du=3000&sqp=CMmxsqwG&rs=AOn4CLB1IRn2k7nmtdRR5dTUlWtXe3Be3g'
+              />
+          <VideoCard
+              title='Behind the scenes: An Apple Event shot on iPhone'
+              views='3.6M Views'
+              timestamp='2 months ago'
+              channelImage='https://yt3.ggpht.com/05lhMeAH6tZrPIUsp2yHNz3DwzhKbDUQcxcY0_qeXVyZttR_pktBzw0FcLUSR6D4fVqsEgL3ZO0=s88-c-k-c0x00ffffff-no-rj'
+              channel='Apple'
+              image='https://i.ytimg.com/an_webp/V3dbG9pAi8I/mqdefault_6s.webp?du=3000&sqp=CNydsqwG&rs=AOn4CLBLRMZU9XOrZ0I9zDORPxAO8tXtqA'
+              />
+          <VideoCard
+              title='The Game Awards Controversy, Mario RPG, PlayStation Portal, Last of Us 2 Remaster | Spawncast Ep 345'
+              views='45K Views'
+              timestamp='3 months ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZTHi1dBGJ24rCecx9lsJHBwHfz_ae_6ZnPNWweing=s88-c-k-c0x00ffffff-no-rj'
+              channel='Spwan Wave'
+              image='https://i.ytimg.com/an_webp/G_nChTPL_fs/mqdefault_6s.webp?du=3000&sqp=CPPvsawG&rs=AOn4CLA19APVhhZuSaGoaA3-5HZJTnIRtQ'
+              />
+          <VideoCard
+              title='How to Remember Things | Jim Kwik'
+              views='1.8M Views'
+              timestamp='5 months ago'
+              channelImage='https://yt3.ggpht.com/AmqWc1vXSvmPaiJC-6K5iUkKpryaD85UpaGkz8lTnDDmR8yhNGgIrjy_q524SUQqM4RHnGRNTA=s88-c-k-c0x00ffffff-no-rj'
+              channel='Jim Kwik'
+              image='https://i.ytimg.com/an_webp/Y7m59wRKXe4/mqdefault_6s.webp?du=3000&sqp=CPqhsqwG&rs=AOn4CLDNDH6UEgZjuAnDT69P7Q7lcqo8jQ'
+              />
+          <VideoCard
+              title='Film! Japanese snipers ambushed, met Chinese sharpshooter, instant headshots'
+              views='11M Views'
+              timestamp='5 months ago'
+              channelImage='https://yt3.ggpht.com/_8FXAB9dtFJ4jlAWuoAz9jyvQo9SqzcRgxcDEBja7YR7FHBzBAP6kbo0nhxCoDI_y3d8TDK2SD4=s88-c-k-c0x00ffffff-no-rj'
+              channel='Gun King'
+              image='https://i.ytimg.com/an_webp/HKQD5R35Reo/mqdefault_6s.webp?du=3000&sqp=CIz5sawG&rs=AOn4CLDlMv-FADADnCR2yX2_PS6I9p9mmw'
+              />
+          <VideoCard
+              title='NDERCOVER - Hollywood English Action Full Movie | Dwayne Johnson "The Rock" Superhit Action Movie'
+              views='6.2M Views'
+              timestamp='6 months ago'
+              channelImage='https://yt3.ggpht.com/qMMfadJ_zB6G_KzDafdy-jwgu7RqYjNlRqrNNlsmNgID4Hlui7ArZYmYUR-rBfYqW5CWedfYeZw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Hollywood English Collection'
+              image='https://i.ytimg.com/an_webp/II7GNZ-zS-U/mqdefault_6s.webp?du=3000&sqp=CLipsqwG&rs=AOn4CLCRidscetCtMePAYA_88fa6W8V6Pg'
+              />
+          <VideoCard
+              title='THE DETECTIVE - English Movie | Hollywood Blockbuster English Action Crime Movie HD | Jason Statham'
+              views='29M Views'
+              timestamp='7 months ago'
+              channelImage='https://yt3.ggpht.com/qMMfadJ_zB6G_KzDafdy-jwgu7RqYjNlRqrNNlsmNgID4Hlui7ArZYmYUR-rBfYqW5CWedfYeZw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Hollywood English Collection'
+              image='https://i.ytimg.com/an_webp/NAIzQFZACcw/mqdefault_6s.webp?du=3000&sqp=CISQsqwG&rs=AOn4CLCphcx7LYvwMBE2fBGnR1QrEKnyFg'
+              />
+          <VideoCard
+              title='Beethoven - Symphony No. 6 (Proms 2012)'
+              views='11M Views'
+              timestamp='10 months ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZQILSglbY8nP30I8FHR7tzHzQwjBZQ_UCRX=s88-c-k-c0x00ffffff-no-rj'
+              channel='Mandetriens'
+              image='https://i.ytimg.com/an_webp/aW-7CqxhnAQ/mqdefault_6s.webp?du=3000&sqp=COC6sqwG&rs=AOn4CLAvlb02JDfIIRnibPCj21YTAnMsrw'
+              />
+          <VideoCard
+              title='Dvořák: Symphony No. 9 "From the New World" / Karajan · Berliner Philharmoniker'
+              views='7.8M Views'
+              timestamp='11 months ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZSSUUqalpauE1a9Kr1uKQRN3cPJcUCVD9Z1LRHOXw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Berliner Philharmoniker'
+              image='https://i.ytimg.com/an_webp/P_1N6_O254g/mqdefault_6s.webp?du=3000&sqp=CIjAsqwG&rs=AOn4CLBll2PhXQLi37R_ac67NptHY4ukAQ'
+              />
+          <VideoCard
+              title='CAMPING in the RAIN [ Cosy Vibes, Relaxing sounds, Our dogs birthday, ASMR ]'
+              views='9M Views'
+              timestamp='1 year ago'
+              channelImage='https://yt3.ggpht.com/ytc/AIf8zZTUy8vpaGO6DstYfJ-TICTuV6xqHHA4ulZ-NghdyA=s88-c-k-c0x00ffffff-no-rj'
+              channel='Go4x4'
+              image='https://i.ytimg.com/an_webp/t-amk-VJQXo/mqdefault_6s.webp?du=3000&sqp=CKaxsqwG&rs=AOn4CLDhJqRlo5u_s_Bt6KG5rkRY80gIew'
+              />
+          <VideoCard
+              title='BUILDING A TINY HOUSE - DIY Full Exterior Build'
+              views='17M Views'
+              timestamp='2 years ago'
+              channelImage='https://yt3.ggpht.com/zv_8nj95rvpayYjHZJ27SV-ZTAWEvE29PGyrOX-iKELK4hQT-xlSf8Ra0hjfoN5kV6pdVD0jbw=s88-c-k-c0x00ffffff-no-rj'
+              channel='Drew Builds Stuff'
+              image='https://i.ytimg.com/an_webp/QDLLgV2fgx4/mqdefault_6s.webp?du=3000&sqp=CIjAsqwG&rs=AOn4CLDNaaWJOTOHDK01Vyeprbj7E1vONQ'
+              />
         </div>
     </div>)
 }
